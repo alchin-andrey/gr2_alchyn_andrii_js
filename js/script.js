00031f36b7be07f4461889aa03f08686bfcabb81
alchin-andrey <92293144+alchin-andrey@users.noreply.github.com>
2021-10-27 00:21:13 +0300

    script

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Константы
+const DATE_OF_BIRTH = '18 марта 1988г.';
+const PLACE_OF_BIRTH = 'Украина, Донецкая одл., г. Горловка';
+const GENDER = 'мужской';
+
+
+// Переменные
+let myAge = 33;
+
+
+console.log('Приветствую! Сейчас я Вам поведаю немного о себе.')
+
+// Вывод
+
+// Константы
+console.log('Дата рождения:', DATE_OF_BIRTH, '(' + typeof(DATE_OF_BIRTH) + ');');
+console.log('Место рождения:', PLACE_OF_BIRTH, '(' + typeof(PLACE_OF_BIRTH) + ');');
+console.log('Мой пол:', GENDER, '(' + typeof(GENDER) + ');');
+
+// Переменные
+
+console.log('Мой возраст:', myAge, '(' + typeof(myAge) + ');');
