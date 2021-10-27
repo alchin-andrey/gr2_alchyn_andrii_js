9e5c17437d2f24b24c7a0c15f8b06b86557e15b7
alchin_andrey <immortiale@gmail.com>
2021-10-28 01:17:08 +0300

    js

--- js/script.js
+++ js/script.js
@@ -1,38 +1,44 @@
 // Константы
-const MY_DATE_OF_BIRTH = '18 марта 1988г.';
-const MY_PLACE_OF_BIRTH = 'Украина, Донецкая одл., г. Горловка';
-const MY_GENDER = 'мужской';
-const MY_EYE_COLOR = 'голубой';
 const MY_NAME = 'Андрей';
 const MY_SURNAME = 'Алчин';
 const MY_MIDDLE_NAME = 'Юрьевич';
-
+const MY_DATE_OF_BIRTH = '18 марта 1988г.';
+const MY_PLACE_OF_BIRTH = 'Украина, Донецкая одл., г. Горловка';
+const MY_GENDER = 'мужской';
+const MY_EYE_COLOR = 'озерный';
+const STUDY_PATH_OF_MY_LIFE = Infinity;
+const ME_AND_SMOKE = false;
+const ME_AND_FOOD = true;
 
 // Переменные
 let myAge = 33;
 let myPlaceOfResidence = 'г. Краматорск';
 let myPlaceOfWork = 'архитектурно-строительная компания';
+let myFavoriteSport = 'настольный-тенис, футбол';
+let myWeight = 74;
 let myJavaScriptSkill = 0.01;
 let myGrade = null;
 
-
-
 // Вывод
 console.log('Приветствую! Сейчас я Вам поведаю немного о себе.')
-
-// Константы
-console.log('Дата рождения: ' + MY_DATE_OF_BIRTH + ' (' + typeof(MY_DATE_OF_BIRTH) + ');');
-console.log('Место рождения: ' + MY_PLACE_OF_BIRTH + ' (' + typeof(MY_PLACE_OF_BIRTH) + ');');
-console.log('Мой пол: ' + MY_GENDER + ' (' + typeof(MY_GENDER) + ');');
-console.log('Мой цвет глаз: ' + MY_EYE_COLOR + ' (' + typeof(MY_EYE_COLOR) + ');');
-console.log('Моё имя: ' + MY_NAME + ' (' + typeof(MY_NAME) + ');');
-console.log('Моя фамилия: ' + MY_SURNAME + ' (' + typeof(MY_SURNAME) + ');');
-console.log('Моё отчество: ' + MY_MIDDLE_NAME + ' (' + typeof(MY_MIDDLE_NAME) + ');');
-
-// Переменные
-console.log('Мой возраст: ' + myAge + ' (' + typeof(myAge) + ');');
-console.log('Моё место жительства: ' + myPlaceOfResidence + ' (' + typeof(myPlaceOfResidence) + ');');
-console.log('Моё место работы: ' + myPlaceOfWork + ' (' + typeof(myPlaceOfWork) + ');');
-console.log('Мой скил в JavaScript на данный момент: ' + myJavaScriptSkill * 100 + '%' + ' (' + typeof(myJavaScriptSkill) + ');');
 console.log('--------------------------------------------------------------');
-console.log('Моя оценка за ДЗ: ' + myGrade + ' (' + typeof(myGrade) + ');');
+// Константы
+console.log('Моё имя: ' + MY_NAME + ' (' + typeof(MY_NAME) + ')' + ' [константа];');
+console.log('Моя фамилия: ' + MY_SURNAME + ' (' + typeof(MY_SURNAME) + ')' + ' [константа];');
+console.log('Моё отчество: ' + MY_MIDDLE_NAME + ' (' + typeof(MY_MIDDLE_NAME) + ')' + ' [константа];');
+console.log('Дата рождения: ' + MY_DATE_OF_BIRTH + ' (' + typeof(MY_DATE_OF_BIRTH) + ')' + ' [константа];');
+console.log('Место рождения: ' + MY_PLACE_OF_BIRTH + ' (' + typeof(MY_PLACE_OF_BIRTH) + ')' + ' [константа];');
+console.log('Мой пол: ' + MY_GENDER + ' (' + typeof(MY_GENDER) + ')' + ' [константа];');
+console.log('Цвет моих глаз: ' + MY_EYE_COLOR + ' (' + typeof(MY_EYE_COLOR) + ')' + ' [константа];');
+console.log('Учебный путь моей жизни: ' + STUDY_PATH_OF_MY_LIFE + ' (' + typeof(STUDY_PATH_OF_MY_LIFE) + ')' + ' [константа];');
+console.log('Курю ли я?: ' + ME_AND_SMOKE + ' (' + typeof(ME_AND_SMOKE) + ')' + ' [константа];');
+console.log('Ем ли я?: ' + ME_AND_FOOD + ' (' + typeof(ME_AND_FOOD) + ')' + ' [константа];');
+// Переменные
+console.log('Мой возраст: ' + myAge + ' года' + ' (' + typeof(myAge) + ')' + ' [переменная];');
+console.log('Моё место жительства: ' + myPlaceOfResidence + ' (' + typeof(myPlaceOfResidence) + ')' + ' [переменная];');
+console.log('Моё место работы: ' + myPlaceOfWork + ' (' + typeof(myPlaceOfWork) + ')' + ' [переменная];');
+console.log('Мой любимый вид спорта: ' + myFavoriteSport + ' (' + typeof(myFavoriteSport) + ')' + ' [переменная];');
+console.log('Мой вес: ' + myWeight + ' кг.' + ' (' + typeof(myWeight) + ')' + ' [переменная];');
+console.log('Мой скил в JavaScript на данный момент: ' + myJavaScriptSkill * 100 + '%' + ' (' + typeof(myJavaScriptSkill) + ')' + ' [переменная];');
+console.log('--------------------------------------------------------------');
+console.warn('Моя оценка за ДЗ: ' + myGrade + ' (' + typeof(myGrade) + ')' + ' [переменная].');
