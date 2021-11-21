c97445f11e5730e15845a1cda35d6eb5c1226604
alchin_andrey <immortiale@gmail.com>
2021-11-21 17:39:20 +0200

    10

--- js_homework/homework_10.js
+++ js_homework/homework_10.js
@@ -10,15 +10,19 @@
 
 $(document).ready(function(){
 
+    let start = null;
+    let finish = null;
     $('div').remove();
     document.body.style.cssText = '';
 
     async function receive_request(){
         let response = await fetch('https://jsonplaceholder.typicode.com/users')
         let data = await response.json()
+        start = new Date ();
+        for (i = 0; i < 10; i++) {
         data.forEach((users) => {
         document.body.insertAdjacentHTML('beforeEnd',  `
-            <div class="item">
+            <div class="item" id = ${users.id} >
                 <div class="close"><span>Х</span></div>
                 <div><img src="./img/unnamed.jpg"></div>
                 <div class="text"><span>Id: </span>${users.id}</div>
@@ -30,13 +34,14 @@ $(document).ready(function(){
             `)
         });
     }
+        finish = new Date ();
+        console.log (finish.getTime() - start.getTime())
+    }
 
     function  get_phone_number (element) {
         console.log(element)
         for(let i = 0;i < element.length;i++){
-            console.log(element[i].id)
             element[i].onclick = function (event) {
-                console.log(element[i].id)
                 alert(`Phone number: ${element[i].id}`);
             }
         }
@@ -47,20 +52,20 @@ $(document).ready(function(){
             element[i].onclick = function (event) {
                 let block = this.parentNode;
                 block.style.opacity = 1;
-                let block_opacity = setInterval(function() {
-                    if (block.style.opacity > 0) {
-                        block.style.opacity -= 0.1;
-                    } else {
-                        clearInterval(block_opacity);
-                        block.remove();
-                        console.log(document.getElementsByClassName('btm'));
-                        return document.getElementsByClassName('btm');
-                    }
-                }, 60)
+                if (confirm('Дуйствительно удалить?')) {
+                    let block_opacity = setInterval(function() {
+                        if (block.style.opacity > 0) {
+                            block.style.opacity -= 0.1;
+                        } else {
+                            clearInterval(block_opacity);
+                            block.remove();
+                            return document.getElementsByClassName('btm');
+                        }
+                    }, 60)
+                }
             }
         }
     }
-
     function go(){
         let user_phone_btm = document.getElementsByClassName('btm');
         let close_button = document.getElementsByClassName('close');
@@ -69,23 +74,3 @@ $(document).ready(function(){
     }
     receive_request().then(go);
 });
-
-
-
-
-
-
-
-
-
-// function  close (element) {
-    //     console.log (element);
-    //     for(let i = 0;i < element.length;i++){
-    //         console.log (element[i], element[i].getElementsByClassName('close'));
-    //         element[i].onclick = function (event) {
-    //             this.parentNode.parentNode.removeChild(this.parentNode);
-    //         }
-    //     }
-    // }
-
-
